1e9900e78b6ba8b831dd7a63844a47f65276d66b
Mohit Nirgulkar <60045850+raklugrin01@users.noreply.github.com>
2021-10-30 18:52:05 +0530

    read_data file updated

--- read_data.py
+++ read_data.py
@@ -13,56 +13,29 @@ def read_data():
     names = []
     for root, dirs, files in os.walk(path_to_files):
         for name in files:
-            #identify male
-            if name[0:2] in '0310111215':
-                #Arger (Wut) -> Angry
-                if name[5] == 'W':
-                    emotion.append('male_angry')
-                #Langeweile -> Boredom
-                elif name[5] == 'L':
-                    emotion.append('male_bored')
-                # Ekel -> Disgusted
-                elif name[5] == 'E':
-                    emotion.append('male_disgust')
-                #Angst -> Angry
-                elif name[5] == 'A':
-                    emotion.append('male_fear')
-                #Freude -> Happiness
-                elif name[5] == 'F':
-                    emotion.append('male_happy')
-                #Trauer -> Sadness
-                elif name[5] == 'T':
-                    emotion.append('male_sad')
-                #Neutral
-                elif name[5] == 'N':
-                    emotion.append('male_neutral')
-                else:
-                    emotion.append('unknown')
+            #Arger (Wut) -> Angry
+            if name[5] == 'W':
+                emotion.append('angry')
+            #Langeweile -> Boredom
+            elif name[5] == 'L':
+                emotion.append('bored')
+            # Ekel -> Disgusted
+            elif name[5] == 'E':
+                emotion.append('disgust')
+            #Angst -> Angry
+            elif name[5] == 'A':
+                emotion.append('fear')
+            #Freude -> Happiness
+            elif name[5] == 'F':
+                emotion.append('happy')
+            #Trauer -> Sadness
+            elif name[5] == 'T':
+                emotion.append('sad')
+            #Neutral
+            elif name[5] == 'N':
+                emotion.append('neutral')
             else:
-                #identify female
-                #Arger (Wut) -> Angry
-                if name[5] == 'W':
-                    emotion.append('female_angry')
-                #Langeweile -> Boredom
-                elif name[5] == 'L':
-                    emotion.append('female_bored')
-                # Ekel -> Disgusted
-                elif name[5] == 'E':
-                    emotion.append('female_disgust')
-                #Angst -> Angry
-                elif name[5] == 'A':
-                    emotion.append('female_fear')
-                #Freude -> Happiness
-                elif name[5] == 'F':
-                    emotion.append('female_happy')
-                #Trauer -> Sadness
-                elif name[5] == 'T':
-                    emotion.append('female_sad')
-                #Neutral
-                elif name[5] == 'N':
-                    emotion.append('female_neutral')
-                else:
-                    emotion.append('unknown')
+                emotion.append('unknown')
             names.append(name)
             path.append(os.path.join(path_to_files,name))
         emodb_df = pd.DataFrame(emotion, columns=['labels'])
